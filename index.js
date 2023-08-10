e326bdb9346064a0c64c10436372a895fc3d8455
Akkaraju Satvik <satvik.akkaraju@alcovex.studio>
2023-08-11 00:12:49 +0530

    createBlankVideoWithAudio & createBlankVideo

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const ffprobePath = require('@ffprobe-installer/ffprobe').path;
 const FOLDERS = {
   INPUT: './inputs',
   OUTPUT: './outputs',
-}
+};
 
 ffmpeg.setFfmpegPath(ffmpegPath);
 ffmpeg.setFfprobePath(ffprobePath);
 
 function mergeAudioAndVideo(audio, video) {
   return new Promise((resolve, reject) => {
-    const output = join(FOLDERS.OUTPUT, basename(video));
+    const output = join(FOLDERS.OUTPUT, basename('mergeAudioAndVideo.mkv'));
     ffmpeg()
       .input(audio)
       .input(video)
@@ -28,11 +28,51 @@ function mergeAudioAndVideo(audio, video) {
   });
 }
 
+function createBlankVideo() {
+  return new Promise((resolve, reject) => {
+    const output = join(FOLDERS.OUTPUT, 'createBlankVideo.mkv');
+    ffmpeg()
+      .input('color=black:s=1280x720')
+      .inputFormat('lavfi')
+      .inputOptions(['-t', '10'])
+      .outputOptions('-c:v libx264')
+      .outputOptions('-pix_fmt yuv420p')
+      .output(output)
+      .on('end', () => resolve(output))
+      .on('error', reject)
+      .on('progress', progress => console.log(progress.percent + '% done'))
+      .run();
+  });
+}
+
+function createBlankVideoWithAudio(audio) {
+  return new Promise((resolve, reject) => {
+    const output = join(FOLDERS.OUTPUT, 'createBlankVideoWithAudio.mkv');
+    ffmpeg()
+      .input(audio)
+      .input('color=black:s=1280x720')
+      .inputFormat('lavfi')
+      .inputOptions(['-t', '10'])
+      .outputOptions('-c:v libx264')
+      .outputOptions('-pix_fmt yuv420p')
+      .output(output)
+      .on('end', () => resolve(output))
+      .on('error', reject)
+      .on('progress', progress => console.log(progress.percent + '% done'))
+      .run();
+  });
+}
+
 async function main() {
   const audio = join(FOLDERS.INPUT, 'RTc415cafaa327c4295e94a17b0a838143.mka');
   const video = join(FOLDERS.INPUT, 'RTb12dd52ff5156d2616fc32897a84ba8d.mkv');
   const output = await mergeAudioAndVideo(audio, video);
-  console.log(output);
+  console.log(output, 'mergeAudioAndVideo');
+  const blankVideo = await createBlankVideo();
+  console.log(blankVideo, 'createBlankVideo');
+  const blankVideoWithAudio = await createBlankVideoWithAudio(audio);
+  console.log(blankVideoWithAudio, 'createBlankVideoWithAudio');
+  
 }
 
 main();

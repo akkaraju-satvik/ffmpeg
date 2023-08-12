481a4ab89079aa166f8211a482a083800a7df208
Akkaraju Satvik <satvik.akkaraju@alcovex.studio>
2023-08-12 12:10:34 +0530

    stitch multiple audios with unequal gaps

--- index.js
+++ index.js
@@ -174,11 +174,44 @@ const joinMultipleVideosWithUnequalTimeGap = async (videos, timeGaps, resolution
   });
 }; 
 
+const joinMultipleAudiosWithUnequalTimeGap = async (audios, timeGaps) => {
+  return new Promise((resolve, reject) => {
+    const output = join(FOLDERS.OUTPUT, 'joinMultipleAudiosWithUnequalTimeGap.mp3');
+    const ffmpegCommand = ffmpeg();
+    audios.forEach((audio, index) => {
+      ffmpegCommand.input(audio);
+      if (index !== audios.length - 1) {
+        ffmpegCommand.input(`anullsrc=channel_layout=stereo:sample_rate=44100`);
+        ffmpegCommand.inputFormat('lavfi');
+        ffmpegCommand.inputOptions(['-t', timeGaps[index]]);
+      }
+    });
+    const complexFilter = ''
+    for (let i = 0; i < (audios.length * 2) - 1; i++) {
+      complexFilter.concat(`${i}:a `)
+    };
+    console.log(complexFilter);
+    ffmpegCommand
+      .complexFilter([
+        `${complexFilter} concat=n=${audios.length + audios.length - 1}:v=0:a=1 [a]`,
+      ], ['a'])
+      .outputOptions('-c:v libx264')
+      .outputOptions('-pix_fmt yuv420p')
+      .output(output)
+      .on('end', () => resolve(output))
+      .on('error', reject)
+      .on('progress', progress => console.log(progress.percent + '% done'))
+      .run();
+  });
+};
+
 async function main() {
   const audio = join(FOLDERS.INPUT, 'RTc415cafaa327c4295e94a17b0a838143.mka');
   const video = join(FOLDERS.INPUT, 'RTb12dd52ff5156d2616fc32897a84ba8d.mkv');
   const videoHalf1 = join(FOLDERS.INPUT, 'splitVideoInto2-1.mkv');
   const videoHalf2 = join(FOLDERS.INPUT, 'splitVideoInto2-2.mkv');
+  const audio1 = join(FOLDERS.INPUT, 'RT74c45bdb44e90d0362abeeba44480a00.mka');
+  const audio2 = join(FOLDERS.INPUT, 'RT080fa8dc5d52d340d0c7c8fc6e985313.mka');
   // const output = await mergeAudioAndVideo(audio, video);
   // console.log(output, 'mergeAudioAndVideo');
   // const blankVideo = await createBlankVideo();
@@ -188,11 +221,12 @@ async function main() {
   ffmpeg.ffprobe(video, async (err, metadata) => {
     console.log(`${metadata.streams[0].width}x${metadata.streams[0].height}`, 'video resolution');
     const resolution = `${metadata.streams[0].width}x${metadata.streams[0].height}`;
-    // const joinedMultipleVideosWith10SecondsGap = await joinMultipleVideosWith10SecondsGap([videoHalf1, videoHalf2, video], resolution);
-    // console.log(joinedMultipleVideosWith10SecondsGap, 'join2VideosWith10SecondsGap');
     const joinedMultipleVideosWithUnequalTimeGap = await joinMultipleVideosWithUnequalTimeGap([videoHalf1, videoHalf2, video], [10, 5], resolution);
     console.log(joinedMultipleVideosWithUnequalTimeGap, 'joinMultipleVideosWithUnequalTimeGap');
   });
+  const joinedMultipleAudiosWithUnequalTimeGap = await joinMultipleAudiosWithUnequalTimeGap([audio1, audio2], [20]);
+  console.log(joinedMultipleAudiosWithUnequalTimeGap, 'joinMultipleAudiosWithUnequalTimeGap');
+  
 }
 
 main();
